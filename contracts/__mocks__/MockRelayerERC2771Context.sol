20522ded9206b76fb2fc3f04a4c917c052bf4aeb
gitpusha <gitpusha@gmail.com>
2022-08-25 08:18:17 +0200

    fix: import statements

--- contracts/__mocks__/MockRelayerERC2771Context.sol
+++ contracts/__mocks__/MockRelayerERC2771Context.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-// import {RelayerFeeContext} from "@gelatonetwork/relayer-context/contracts";
+import {
+    RelayerContext
+} from "@gelatonetwork/relayer-context/contracts/RelayerContext.sol";
 
 // /*is RelayerFeeContext*/
 // contract MockRelayerFeeContext {
