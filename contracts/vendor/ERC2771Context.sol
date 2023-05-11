5933227843737c2abdfb00cbf1309bb88a8142d4
Denis Pingin <2085934+denis-pingin@users.noreply.github.com>
2023-05-11 10:07:03 +0200

    chore: update GelatoRelayERC2771 addresses and add zkSync support (#16)
    * chore: support other Relay addresses on zkSync
    
    * chore: update GelatoRelayERC2771 addresses
    
    * chore: update .gitignore
    
    * chore: remove dependency from Context.sol
    
    * v3.0.0

--- contracts/vendor/ERC2771Context.sol
+++ contracts/vendor/ERC2771Context.sol
@@ -3,13 +3,11 @@
 
 pragma solidity ^0.8.1;
 
-import "./Context.sol";
-
 /**
  * @dev Context variant with ERC2771 support.
  */
-// copied from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/metatx/ERC2771Context.sol
-abstract contract ERC2771Context is Context {
+// based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/metatx/ERC2771Context.sol
+abstract contract ERC2771Context {
     address private immutable _trustedForwarder;
 
     constructor(address trustedForwarder) {
@@ -25,13 +23,7 @@ abstract contract ERC2771Context is Context {
         return forwarder == _trustedForwarder;
     }
 
-    function _msgSender()
-        internal
-        view
-        virtual
-        override
-        returns (address sender)
-    {
+    function _msgSender() internal view virtual returns (address sender) {
         if (isTrustedForwarder(msg.sender)) {
             // The assembly code is more direct than the Solidity version using `abi.decode`.
             /// @solidity memory-safe-assembly
@@ -39,21 +31,15 @@ abstract contract ERC2771Context is Context {
                 sender := shr(96, calldataload(sub(calldatasize(), 20)))
             }
         } else {
-            return super._msgSender();
+            return msg.sender;
         }
     }
 
-    function _msgData()
-        internal
-        view
-        virtual
-        override
-        returns (bytes calldata)
-    {
+    function _msgData() internal view virtual returns (bytes calldata) {
         if (isTrustedForwarder(msg.sender)) {
             return msg.data[:msg.data.length - 20];
         } else {
-            return super._msgData();
+            return msg.data;
         }
     }
 }
